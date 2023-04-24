adc7c2e1eb3b6a8b2e5cd621dc1aff935a80de54
Kagiso Semenya <kagiso@rhest.studio>
2023-04-24 16:50:47 +0200

    What We Do

---
+++ src/features/pages/SingleService.jsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const SingleService = () => {
+  return (
+    <Box height="95vh">
+        <Heading as="h3" variant="h3">
+            Creative Strategy  
+        </Heading>
+        <Flex flexDirection="column">
+            <Flex>
+                Hello
+            </Flex>
+            <Flex>
+                Hello
+            </Flex>
+        </Flex>
+    </Box>
+  )
+}
+
+export default SingleService
